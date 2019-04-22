230399707f763862d57bb53b0200e7a9cf6bdc22
Brett Sheleski <brett@sheleski.com>
2019-04-22 14:29:22 -0500

    stuff

--- CanvasApp.Mobile/CanvasApp.Mobile/MainPage.xaml.cs
+++ CanvasApp.Mobile/CanvasApp.Mobile/MainPage.xaml.cs
@@ -32,9 +32,7 @@ namespace CanvasApp.Mobile
             Complex p1 = new Complex(-1.5, 1);
             Complex p2 = new Complex(0.5, -1);
 
-            var tasks = DrawMandelbrotAsync(canvas, info);
-
-            Task.WaitAll(tasks.ToArray());
+            DrawMandelbrot(canvas, info);
         }
 
         void SetColors()
@@ -60,8 +58,7 @@ namespace CanvasApp.Mobile
             set { SetValue(CanvasBoundsProperty, value); }
         }
 
-        public static readonly BindableProperty CanvasBoundsProperty = BindableProperty.Create(nameof(CanvasBounds), typeof(RectangleF), typeof(MainPage), new RectangleF(new PointF(-1.5f, 1), new SizeF(2, -2)), BindingMode.TwoWay, propertyChanged: OnCanvasBoundsPropertyChanged);
-        private SKMatrix _canvasInvertMatrix;
+        public static readonly BindableProperty CanvasBoundsProperty = BindableProperty.Create(nameof(CanvasBounds), typeof(RectangleF), typeof(MainPage), new RectangleF(new PointF(-1.5f, -1), new SizeF(2, 2)), BindingMode.TwoWay, propertyChanged: OnCanvasBoundsPropertyChanged);
         private long _maxIterations = 100;
 
         SKColor[] _colors = new SKColor[] { };
@@ -76,120 +73,116 @@ namespace CanvasApp.Mobile
             Canvas.InvalidateSurface();
         }
 
-        IEnumerable<Task> DrawMandelbrotAsync(SKCanvas canvas, SKImageInfo info)
+        void DrawMandelbrot(SKCanvas canvas, SKImageInfo info)
         {
-            Complex p1 = new Complex(CanvasBounds.Left, CanvasBounds.Top),
-                p2 = new Complex(CanvasBounds.Right, CanvasBounds.Bottom);
 
-            float minR = (float)Math.Min(p1.Real, p2.Real),
-                maxR = (float)Math.Max(p1.Real, p2.Real),
-                minI = (float)Math.Min(p1.Imaginary, p2.Imaginary),
-                maxI = (float)Math.Max(p1.Imaginary, p2.Imaginary);
+            int divisionsX = 1,
+                divisionsY = 16,
+                width = info.Width / divisionsX,
+                height = info.Height / divisionsY;
 
-            float offsetX, offsetY, scaleX, scaleY;
+            float complexWidth = CanvasBounds.Width / divisionsX,
+                complexHeight = CanvasBounds.Height / divisionsY;
 
-            offsetX = info.Width / 2f;
-            offsetY = info.Height / 2f;
+            List<Task<BitmapInfo>> tasks = new List<Task<BitmapInfo>>(divisionsY * divisionsX);
 
-            scaleX = info.Width / (maxR - minR);
-            scaleY = info.Height / (maxI - minI);
-
-            PointF center = new PointF((minR + maxR) / 2f, (minI + maxI) / 2f);
-
-            float scale = Math.Min(scaleX, scaleY);
-
-            canvas.Translate(offsetX, offsetY);
-            canvas.Scale(scale, -scale);
-            canvas.Translate(-center.X, -center.Y);
-
-            if (canvas.TotalMatrix.TryInvert(out _canvasInvertMatrix))
+            
+            for (int x = 0; x < divisionsX; ++x)
             {
-                var pTemp = _canvasInvertMatrix.MapPoint(minR, maxI);
-
-                minR = pTemp.X;
-                maxI = pTemp.Y;
-
-
-                pTemp = _canvasInvertMatrix.MapPoint(maxR, minI);
-
-                maxR = pTemp.X;
-                maxI = pTemp.Y;
-
-
-            }
-
-
-            float step = 1 / scale;
-
-            int divisionsH = 4, divisionsW = 4;
-
-            float chunkW = Math.Abs(CanvasBounds.Width / divisionsW);
-            float chunkH = Math.Abs(CanvasBounds.Height / divisionsH);
-
-            for (int h = 0; h < divisionsH; ++h)
-            {
-                for (int w = 0; w < divisionsW; ++w)
+                for (int y = 0; y < divisionsY; ++y)
                 {
-                    // avoid closure issues.
-                    int ww = w;
-                    int hh = h;
+                    System.Drawing.Rectangle drawingRect = new System.Drawing.Rectangle(info.Rect.Left + (x * width), info.Rect.Top + (y * height), width, height);
+                    RectangleF complexRect = new RectangleF(CanvasBounds.Left + (x * complexWidth), CanvasBounds.Top + (y * complexHeight), complexWidth, complexHeight);
 
-                    
-                    yield return Task.Run(() =>
+                    tasks.Add(Task<BitmapInfo>.Run(() =>
                     {
-                        float startR = minR + (ww * chunkW),
-                            endR = startR + chunkW,
-                            startI = minI + (hh * chunkH),
-                            endI = startI + chunkH;
-
-
-                        long rank;
-
-                        for (float r = startR; r < endR; r += step)
+                        return new BitmapInfo
                         {
-                            for (float i = startI; i < endI; i += step)
-                            {
-                                rank = GetRank(r, i, _maxIterations);
-
-                                if (rank == _maxIterations)
-                                {
-                                    canvas.DrawPoint(r, i, Xamarin.Forms.Color.Black.ToSKColor());
-                                }
-                                else
-                                {
-                                    canvas.DrawPoint(r, i, _colors[rank]);
-                                }
-                            }
-                        }
-                    });
+                            Bitmap = GetMandelbrotBitmap(complexRect, drawingRect),
+                            Point = new SKPoint(drawingRect.X, drawingRect.Y)
+                        };
+                    }));
                 }
             }
 
-            //for (float r = minR; r < maxR; r += step)
-            //{
-            //    for (float i = minI; i < maxI; i += step)
-            //    {
-            //        p = new SKPoint(r, i);
+            Task.WhenAll(tasks).Wait();
 
-            //        rank = GetRank(p.X, p.Y, _maxIterations);
-
-            //        if (rank == _maxIterations)
-            //        {
-            //            canvas.DrawPoint(p, Xamarin.Forms.Color.Black.ToSKColor());
-            //        }
-            //        else
-            //        {
-            //            canvas.DrawPoint(p, _colors[rank]);
-            //        }
-            //    }
-            //}
-
-            //return Task.CompletedTask;
+            foreach (var t in tasks)
+            {
+                canvas.DrawBitmap(t.Result.Bitmap, t.Result.Point);
+            }
         }
 
-        long GetRank(double real, double imaginary, long maxN)
+        struct BitmapInfo
         {
-            double r = 0, i = 0, xnew, ynew;
+            public SKBitmap Bitmap { get; set; }
+            public SKPoint Point { get; set; }
+        }
+
+        
+        public struct RectangleD
+        {
+            public RectangleD(decimal left, decimal top, decimal right, decimal bottom)
+            {
+                Left = left;
+                Top = top;
+                Right = right;
+                Bottom = bottom;
+            }
+
+            public decimal Left { get; }
+            public decimal Right { get;}
+
+            public decimal Top { get; }
+
+            public decimal Bottom { get; }
+
+            public decimal Width { get { return Right - Left; } }
+            public decimal Height { get { return Top - Bottom; } }
+
+        }
+
+        SKBitmap GetMandelbrotBitmap(RectangleF complexBounds, System.Drawing.Rectangle drawingBounds)
+        {
+            
+
+            SKBitmap bmp = new SKBitmap(drawingBounds.Width, drawingBounds.Height);
+
+            float stepR = complexBounds.Width / drawingBounds.Width,
+                stepI = complexBounds.Height / drawingBounds.Height;
+
+            float scaleR = drawingBounds.Width / complexBounds.Width,
+                scaleI = drawingBounds.Height / complexBounds.Height;
+
+            float step = Math.Max(stepR, stepI);
+
+            PointF offset = new PointF(
+                drawingBounds.X - (complexBounds.X * scaleR),
+                drawingBounds.Y - (complexBounds.Y * scaleI)
+                );
+
+            var px = from y in Enumerable.Range(drawingBounds.Top, drawingBounds.Bottom)
+                     from x in Enumerable.Range(drawingBounds.Left, drawingBounds.Right)
+                     select GetColorFromRank(GetRank((x - offset.X) * step, (y - offset.Y) * step, _maxIterations), _colors);
+
+            bmp.Pixels = px.ToArray();
+
+            return bmp;
+        }
+
+        static readonly SKColor black = Xamarin.Forms.Color.Black.ToSKColor();
+
+        SKColor GetColorFromRank(long rank, SKColor[] colors)
+        {
+            if (rank < colors.LongLength)
+                return colors[rank];
+
+            return black;
+        }
+
+        long GetRank(float real, float imaginary, long maxN)
+        {
+            float r = 0, i = 0, xnew, ynew;
             long n;
 
             for (n = 0; n < maxN; ++n)
@@ -205,19 +198,100 @@ namespace CanvasApp.Mobile
             return maxN;
         }
 
+
+        Dictionary<long, SKPoint> touchDictionary = new Dictionary<long, SKPoint>();
+        long touchId = -1;
+        SKPoint previousPoint;
+
         private void Canvas_Touch(object sender, SKTouchEventArgs e)
         {
+
+            //switch (e.ActionType)
+            //{
+            //    case SKTouchAction.Pressed:
+            //        touchDictionary[e.Id] = e.Location;
+            //        break;
+            //    case SKTouchAction.Moved:
+            //        if (touchDictionary.ContainsKey(e.Id))
+            //        {
+            //            // Single-finger drag
+            //            if (touchDictionary.Count == 1)
+            //            {
+            //                SKPoint prevPoint = touchDictionary[e.Id];
+
+                            
+
+
+            //                // Adjust the matrix for the new position
+            //                matrix.TransX += point.X - prevPoint.X;
+            //                matrix.TransY += point.Y - prevPoint.Y;
+            //                Canvas.InvalidateSurface();
+            //            }
+            //            // Double-finger scale and drag
+            //            else if (touchDictionary.Count >= 2)
+            //            {
+            //                // Copy two dictionary keys into array
+            //                long[] keys = new long[touchDictionary.Count];
+            //                touchDictionary.Keys.CopyTo(keys, 0);
+
+            //                // Find index of non-moving (pivot) finger
+            //                int pivotIndex = (keys[0] == e.Id) ? 1 : 0;
+
+            //                // Get the three points involved in the transform
+            //                SKPoint pivotPoint = touchDictionary[keys[pivotIndex]];
+            //                SKPoint prevPoint = touchDictionary[e.Id];
+            //                SKPoint newPoint = point;
+
+            //                // Calculate two vectors
+            //                SKPoint oldVector = prevPoint - pivotPoint;
+            //                SKPoint newVector = newPoint - pivotPoint;
+
+            //                // Scaling factors are ratios of those
+            //                float scaleX = newVector.X / oldVector.X;
+            //                float scaleY = newVector.Y / oldVector.Y;
+
+            //                if (!float.IsNaN(scaleX) && !float.IsInfinity(scaleX) &&
+            //                    !float.IsNaN(scaleY) && !float.IsInfinity(scaleY))
+            //                {
+            //                    // If something bad hasn't happened, calculate a scale and translation matrix
+            //                    SKMatrix scaleMatrix =
+            //                        SKMatrix.MakeScale(scaleX, scaleY, pivotPoint.X, pivotPoint.Y);
+
+            //                    SKMatrix.PostConcat(ref matrix, scaleMatrix);
+            //                    Canvas.InvalidateSurface();
+            //                }
+            //            }
+
+            //            // Store the new point in the dictionary
+            //            touchDictionary[e.Id] = e.Location;
+            //        }
+
+            //        break;
+
+            //    case SKTouchAction.Released:
+            //    case SKTouchAction.Cancelled:
+            //        touchDictionary.Remove(e.Id);
+            //        break;
+            //}
+
+
             if (e.ActionType == SKTouchAction.Pressed)
             {
-                //               _maxIterations *= 10;
-                //             SetColors();
+                float pw = e.Location.X / Canvas.CanvasSize.Width;
+                float ph = e.Location.Y / Canvas.CanvasSize.Height;
 
-                var newCenter = _canvasInvertMatrix.MapPoint(e.Location);
+                PointF center = new PointF(
+                                        CanvasBounds.Left + (pw * CanvasBounds.Width),
+                                        CanvasBounds.Top + (ph * CanvasBounds.Height)
+                                        );
 
-                RectangleF b = new RectangleF(newCenter.X - CanvasBounds.Width / 4, newCenter.Y - CanvasBounds.Height / 4, CanvasBounds.Width / 2, CanvasBounds.Height / 2);
+                float quarterWidth = CanvasBounds.Width / 4,
+                    quarterHeight = CanvasBounds.Height / 4;
 
+                
 
-                this.CanvasBounds = b;
+                CanvasBounds = new RectangleF(center.X - quarterWidth, center.Y - quarterHeight, 2 * quarterWidth, 2 * quarterHeight);
+
             }
         }
     }
